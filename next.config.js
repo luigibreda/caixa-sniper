a583e424a5cbd1e4e483b46fae72b70f4764ba21
Luigi Breda <101072609+luigibreda@users.noreply.github.com>
2025-10-28 10:09:06 -0300

    Updated next.config.js

--- next.config.js
+++ next.config.js
@@ -1,4 +1,9 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  swcMinify: false, // desativa SWC
+  compiler: {
+    removeConsole: false,
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
